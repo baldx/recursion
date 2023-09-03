c296ed76fdadda6af7bba6a93190028f9d1215cb
baldx <mr.ahmad.m2006@gmail.com>
2023-09-03 20:22:41 +0200

    create sortMerge function

--- merge.js
+++ merge.js
@@ -1,25 +1,9 @@
-const list = [
-    [5],
-    [6],
-    [4],
-    [3],
-    [7],
-    [2],
-    [9],
-    [8],
-];
+function sortMerge (arr) {
+    if (arr.length === 0) return "Provide an array" //if array is empty return string
+    if (arr.length === 1) return 1; // if array has 1 index return 1
 
-function mergeSort (first, last) {
+    const middle = Math.floor(arr.length / 2); // middle is equals to the array length divide by 2
+    const left = arr.slice(0, middle); //left half of the array is divided
+    const right = arr.slice(middle, arr.length) // right half of the array is divided
 
-    if (last === 1) return 1;
-
-    if (first < last) {
-        let middle = Math.round((first + last) / 2);
-
-        console.log(middle)
-        mergeSort(first, middle)
-
-    } else console.log("a");;
 }
-
-mergeSort(list.indexOf(list[0]), list.indexOf(list[7]));
